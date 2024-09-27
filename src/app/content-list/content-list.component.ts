997d112507d985330e80a1c7ed989750ad58849b
Douglas Cassidy <W9839438@myscc.ca>
2024-09-27 12:19:52 -0400

    - Added content-list-component tag to app.component.html - Created an array with more than 4 content items - Added 4 content-list-item.component.ts tags - Added Input property in content-list-item.component.ts

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {ContentListItemComponent} from "../content-list-item/content-list-item.component";
 
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ContentListItemComponent
+  ],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.css'
 })
 export class ContentListComponent {
+  array: string[] = ["Java", "Javascript Frameworks", "PHP & MYSQL", "Portfolio Development"];
 
 }
