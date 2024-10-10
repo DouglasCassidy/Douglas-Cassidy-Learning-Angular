55b4cfd9ff0969f7373938815d5356baeb202007
Douglas Cassidy <W9839438@myscc.ca>
2024-10-10 15:56:08 -0400

    - Added courses route and courses/id route - Invoked Routes into boostrapApplication

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes} from "@angular/router";
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {routes} from "./app/app.routes";
 import {ContentListComponent} from "./app/content-list/content-list.component";
 import {ContentListItemComponent} from "./app/content-list-item/content-list-item.component";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path:"courses", component: ContentListComponent
+  },
+  {
+    path:"courses/:id", component: ContentListItemComponent
+  }
+]
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)]
