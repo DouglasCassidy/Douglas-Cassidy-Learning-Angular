fa6386ce2bb567d2453ad99cc5870130e9047a64
Douglas Cassidy <W9839438@myscc.ca>
2024-10-10 15:50:38 -0400

    Imported provideRouter, Routes to main.ts - setup router in main.ts to the bootstrapApplication

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes} from "@angular/router";
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {routes} from "./app/app.routes";
+import {ContentListComponent} from "./app/content-list/content-list.component";
+import {ContentListItemComponent} from "./app/content-list-item/content-list-item.component";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const routes: Routes = []
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log("Boostrap Completed"));
